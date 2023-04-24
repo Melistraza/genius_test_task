6ed95e24d4d965a1956f3d6ac5b5896353af3941
melistraza <melistraza@gmail.com>
2023-04-24 12:53:57 +0300

    add handshake unit tests

--- server/unit_tests.py
+++ server/unit_tests.py
@@ -12,27 +12,59 @@ from unittest.mock import MagicMock
 
 class WebTransportProtocolTestCase(TestCase):
 
-    def test_send_response(self):
-        web_protocol = WebTransportProtocol(quic=MagicMock())
-        web_protocol._http = MagicMock()
-        web_protocol._send_response(1, 200, end_stream=False)
-        web_protocol._http.send_headers.assert_called_once_with(
+    def setUp(self) -> None:
+        self.web_protocol = WebTransportProtocol(quic=MagicMock())
+        self.web_protocol._http = MagicMock()
+
+    def test_send_response_200(self):
+        self.web_protocol._send_response(1, 200, end_stream=False)
+        self.web_protocol._http.send_headers.assert_called_once_with(
             stream_id=1,
             headers=[(b':status', b'200'), (b'sec-webtransport-http3-draft', b'draft02')],
             end_stream=False
         )
 
+    def test_send_response_400(self):
+        self.web_protocol._send_response(1, 400, end_stream=True)
+        self.web_protocol._http.send_headers.assert_called_once_with(
+            stream_id=1,
+            headers=[(b':status', b'400')],
+            end_stream=True
+        )
+
     def test_handshake_webtransport(self):
-        web_protocol = WebTransportProtocol(quic=MagicMock())
-        web_protocol._http = MagicMock()
         headers = {
             b":authority": None,
             b":path": None
         }
-        web_protocol._handshake_webtransport(1, headers)
-        web_protocol._http.send_headers.assert_called_once_with(
+        self.web_protocol._handshake_webtransport(1, headers)
+        self.web_protocol._http.send_headers.assert_called_once_with(
             stream_id=1,
             headers=[(b':status', b'400')],
+            end_stream=True
+        )
+
+    def test_handshake_webtransport_wt(self):
+        headers = {
+            b":authority": 'dummy',
+            b":path": b"/wt"
+        }
+        self.web_protocol._handshake_webtransport(1, headers)
+        self.web_protocol._http.send_headers.assert_called_once_with(
+            stream_id=1,
+            headers=[(b':status', b'200'), (b'sec-webtransport-http3-draft', b'draft02')],
+            end_stream=False
+        )
+
+    def test_handshake_webtransport_non_exist_url(self):
+        headers = {
+            b":authority": 'dummy',
+            b":path": b"/dummy"
+        }
+        self.web_protocol._handshake_webtransport(1, headers)
+        self.web_protocol._http.send_headers.assert_called_once_with(
+            stream_id=1,
+            headers=[(b':status', b'404')],
             end_stream=True
         )
 
